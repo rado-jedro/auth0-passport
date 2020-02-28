da5db87ed8323bcba451a775073a81e32a86c50a
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-28 11:05:27 +0000

    Add new sub pages after loging

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,12 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+  if (req.user) {
+    res.render('logged');
+  } else {
+    res.redirect('/user/no-permission');
+  }
 });
 
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/profile', (req, res) => {
+  if (req.user) {
+    res.render('profile');
+  } else {
+    res.redirect('/user/no-permission');
+  }
+});
 
+router.get('/profile/settings', (req, res) => {
+  if (req.user) {
+    res.render('profileSettings');
+  } else {
+    res.redirect('/user/no-permission');
+  }
+});
 module.exports = router;
