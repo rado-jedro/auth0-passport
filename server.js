36f2b0fccafe9a7f74dddfae59162967b66395f2
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-28 11:01:02 +0000

    Add routes

--- server.js
+++ server.js
@@ -3,37 +3,23 @@ const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
 const passport = require('passport');
-const passportConfig = require('./config/passport');
 const session = require('express-session');
+const passportSetup = require('./config/passport');
 
 const app = express();
 
-//Express init session
+// set handlebars as view engine
+app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
+// init session mechanism
 app.use(session({ secret: 'anything' }));
 
 // init passport
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] })
-);
-
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-    res.redirect('/user/logged');
-  }
-);
-
-app.engine(
-  'hbs',
-  hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' })
-);
-app.set('view engine', '.hbs');
-
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,13 +29,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
