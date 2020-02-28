cebe38868430e218566c401c4092f8693a4356ee
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-28 10:41:30 +0000

    Add Passport provider options

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
+
+// configure passport provider options
+passport.use(new GoogleStrategy({
+  clientID: 'clientId',
+  clientSecret: 'clientSecret',
+  callbackURL: 'http://localhost:8000/auth/callback'
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
